89c24f4c91edcf0a2cf91bc2ca7f3f17d1aa44e5
yan <a0726h77@gmail.com>
2015-12-04 20:00:46 +0800

    Fix: meta title

--- resources/views/store/show.blade.php
+++ resources/views/store/show.blade.php
@@ -3,7 +3,7 @@
 @section('meta')
     <meta property="og:url"           content="{{ "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']  }}" />
     <meta property="og:type"          content="website" />
-    <meta property="og:title"         content="{{ $store->title }}" />
+    <meta property="og:title"         content="{{ $store->storeName }}" />
     <meta property="og:description"   content="{{ $store->content }}" />
     <meta property="og:image"         content="http://bazaars.tw/_images/00_logo.png" />
 @endsection
