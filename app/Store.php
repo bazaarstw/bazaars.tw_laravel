941ba36efde0d12a40a094cc8e0bb167c1d305d9
yan <a0726h77@gmail.com>
2015-12-04 19:59:22 +0800

    Show store full address

--- app/Store.php
+++ app/Store.php
@@ -9,6 +9,16 @@ class Store extends Model
     protected $table = 'store';
     protected $primaryKey = 'storeId';
 
+    public function city_name()
+    {
+        return $this->hasOne('App\City', 'cityId', 'city');
+    }
+
+    public function town_name()
+    {
+        return $this->hasOne('App\Town', 'townId', 'town');
+    }
+
     public function meta()
     {
         return $this->hasMany('App\StoreMeta', 'storeId', 'storeId');
